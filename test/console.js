7a3e28dba6d834cb10e5838ae7c578aff802d0c0
Stéphane GUILLY <stephane.guilly@laposte.net>
2018-11-13 11:04:52 +0100

    test :  create basic tests

--- test/console.js
+++ test/console.js
@@ -1,27 +1,26 @@
-const test = require('ava');
-const Logger = require('..')
-
-let log = Logger.getChild('test')
-
-const Hook = require('console-hook');
-
-
-test.before(() => {
-	console.log('before: ')
-});
-
+const test = require('ava')
+const log = require('..').logger
 
 test('test basic', (t) => {
+  log.trace('toto')
+  log.debug('toto')
+  log.info('toto')
+  log.warn('toto')
+  log.error('toto')
+  log.fatal('toto')
 
-   log.trace('toto')  
-   log.debug('toto')  
-   log.info('toto')  
-   log.warn('toto')  
-   log.error('toto')  
-   log.fatal('toto')  
+  t.pass()
+})
 
-   t.pass()
+test('test child', (t) => {
+  const childLog = log.child({ child: 'childLog' })
 
-   
-	
-});
+  childLog.trace('msg child')
+  childLog.debug('msg child')
+  childLog.info('msg child')
+  childLog.warn('msg child')
+  childLog.error('msg child')
+  childLog.fatal('msg child')
+
+  t.pass()
+})
